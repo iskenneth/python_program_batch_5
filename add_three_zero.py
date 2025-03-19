8bb540f96264f403fd48fbd8ddabe6027aa7c6f9
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 20:45:14 +0800

    added integer function

--- add_three_zero.py
+++ add_three_zero.py
@@ -1,7 +1,7 @@
 #Ask the user to input a number (0-1000).
 while True:
     try:
-        number = input("Enter a numner from 1-1000:")
+        number = int(input("Enter a numner from 1-1000:"))
     except ValuerError:
         print ("79&7$+#;_('+;$*)")
 #Add leading zeros to make it 6 digits.
