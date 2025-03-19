a7b3d24f15c0dce4bf1ec1d55b37fa7a21cee007
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 21:00:31 +0800

    input name

--- convert_to_uppercase.py
+++ convert_to_uppercase.py
@@ -1,6 +1,7 @@
 #Ask the user to input their full name.
 while True:
     try:
+        pangalan = input("Enter your full name: ")
     except ValueError:
         print("ERROR 404!!!")
 #Convert all letters to uppercase.
