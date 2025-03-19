43c926a85e8d854ce0c0ab71849d6df82431fb13
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 21:35:03 +0800

    pseudocode

---
+++ reverse_letters_casing.py
@@ -0,0 +1,7 @@
+#Ask the user to input their full name
+while True:
+    try:
+    except ValueError:
+        print("hindi pwede yan ngaaaa!!!!")
+#Change uppercase letters to lowercase and vice versa.
+#Print the result.
