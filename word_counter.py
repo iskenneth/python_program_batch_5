fc71b2d7e140b999b548d6218ba7068127a68d6b
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 21:53:16 +0800

    count numbers

--- word_counter.py
+++ word_counter.py
@@ -1,8 +1,9 @@
 #Ask the user to input a sentence.
 while True:
     try:
-        
+        sentence = input("Enter a statement: ")
+        word_in_sentence = len(sentence.split())#Count the number of words.
     except ValueError:
         print("ERROR!!!")
-#Count the number of words.
+
 #Print the result.
