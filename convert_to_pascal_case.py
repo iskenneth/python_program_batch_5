3afddb5563399aa53a42d1953f307c1ccf7b43f7
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 22:20:22 +0800

    print output

--- convert_to_pascal_case.py
+++ convert_to_pascal_case.py
@@ -3,7 +3,9 @@ while True:
     try:
         name = input("Enter your Full name: ")
         words = name.lower().split() #convert input to lower case 
-        pascal_case = ''.join(words.capitalize () for word in words) #convert to pascal case
+        pascal_case = ''.join(word.capitalize () for word in words) #convert to pascal case
+        print ("Output: ", pascal_case)
+        break
     except ValueError:
         print("ERROR!!")
 
