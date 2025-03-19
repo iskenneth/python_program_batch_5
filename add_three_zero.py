95de1a5fd8f0944f1d0da4f0d9510726476c626b
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 20:40:24 +0800

    ValueError

--- add_three_zero.py
+++ add_three_zero.py
@@ -1,3 +1,8 @@
 #Ask the user to input a number (0-1000).
+while True:
+    try:
+        
+    except ValuerError:
+        print ("79&7$+#;_('+;$*)")
 #Add leading zeros to make it 6 digits.
 #Print the result.
