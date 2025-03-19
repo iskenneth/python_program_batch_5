ac69b846317a24cea17f387d1cde56a4aa98b2af
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 22:19:57 +0800

    break function

--- character_counter.py
+++ character_counter.py
@@ -4,5 +4,6 @@ while True:
         pangalan = input("Enter your Full name:")
         #Print the result.
         print("Count: ", len(pangalan)) #Count the total number of characters, including spaces.
+        break
     except ValueError:
         print("Wrong character")
