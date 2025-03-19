68c60b40562ba7fed5c5f29820becd6c1684cb25
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 22:01:35 +0800

    Valueerror

--- character_counter.py
+++ character_counter.py
@@ -1,3 +1,7 @@
 #Ask the user to input their full name.
+while True:
+    try:
+    except ValueError:
+        print("Wrong character")
 #Count the total number of characters, including spaces.
 #Print the result.
