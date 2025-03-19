3494d257b297308f85fab2899528df67db37fabe
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 20:11:19 +0800

    input code

--- remove_spaces.py
+++ remove_spaces.py
@@ -1,6 +1,7 @@
 #Ask the user to input their full name with spaces.
 while True:
     try:
+        fullname = input ("Enter your Full Name WITH SPACES: ")
     except ValueError:
         print("Error 404!!")
 #Remove spaces at the beginning.
