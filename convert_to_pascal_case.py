72c7487016f296cfc6dbe20ff6843f4be8c83889
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 22:12:58 +0800

    enter full name

--- convert_to_pascal_case.py
+++ convert_to_pascal_case.py
@@ -1,4 +1,9 @@
 #Ask the user to input their full name.
+while True:
+    try:
+        name = input("Enter your Full name: ")
+    except ValueError:
+        print("ERROR!!")
 #Convert all words to lowercase.
 #Capitalize the first letter of each word.
 #Remove spaces between words.
