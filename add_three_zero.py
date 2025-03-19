5994af2b5e4108151b031ac27521556a90b17f66
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 20:52:01 +0800

    Added break function

--- add_three_zero.py
+++ add_three_zero.py
@@ -2,8 +2,9 @@
 while True:
     try:
         number = int(input("Enter a numner from 1-1000:"))
-        print ("Output:", f"{number : 06d}")
+        #Print the result.
+        print ("Output:", f"{number : 06d}") #Add leading zeros to make it 6 digits.
+        break
     except ValuerError:
         print ("79&7$+#;_('+;$*)")
-#Add leading zeros to make it 6 digits.
-#Print the result.
+
