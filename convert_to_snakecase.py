1337fc83ff76f18c5b5eeef8b5a5e78b86098a8d
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 22:31:25 +0800

    print output

--- convert_to_snakecase.py
+++ convert_to_snakecase.py
@@ -3,4 +3,6 @@ fullname = input("Enter your full name: ")
 #Convert all letters to lowercase.
 words = fullname.lower().split()
 #Replace spaces with underscores.
+snake_case= '_'.join(words)
 #Print the result.
+print ("Output", snake_case)
