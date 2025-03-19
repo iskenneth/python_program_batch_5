5254c59b0cfe5ab1611b01a2c860a26f2966bf4a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 21:12:45 +0800

    Input

--- convert_to_lowercase.py
+++ convert_to_lowercase.py
@@ -1,6 +1,7 @@
 #Ask the user to input their full name.
 while True:
     try:
+        pagkakakilanlan = input("Enter your full name: ")
     except ValueError:
         print("..............")
 #Convert all letters to lowercase.
