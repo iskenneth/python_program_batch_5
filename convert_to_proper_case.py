8bd01c8276841eca6c34b092a0b0fd7f32635c10
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 21:20:43 +0800

    enter inout

--- convert_to_proper_case.py
+++ convert_to_proper_case.py
@@ -1,6 +1,7 @@
 #Ask the user to input their full name.
 while True:
     try:
+        full_name = input("Enter your Full name:")
     except ValueError:
         print("ErR0r 404!!")
 #Capitalize the first letter of each word.
