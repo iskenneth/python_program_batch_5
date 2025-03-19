002181548cf59ba856885c6e577004936359ac73
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 21:38:34 +0800

    input full name

--- reverse_letters_casing.py
+++ reverse_letters_casing.py
@@ -1,6 +1,7 @@
 #Ask the user to input their full name
 while True:
     try:
+        pangalan = input("Enter your Full name (in any casing you want: ")
     except ValueError:
         print("hindi pwede yan ngaaaa!!!!")
 #Change uppercase letters to lowercase and vice versa.
