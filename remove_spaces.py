66a56da27b7dd512d87a5576bd2443a6c7596c4a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 20:16:49 +0800

    Remove the spaces

--- remove_spaces.py
+++ remove_spaces.py
@@ -1,9 +1,9 @@
 #Ask the user to input their full name with spaces.
 while True:
     try:
-        fullname = input ("Enter your Full Name WITH SPACES: ")
-        print("Output:")
+        fullname = input ("Enter your Full Name WITH SPACES:")
+        #Print the result.
+        print("Output:" , fullname.lstrip())#Remove spaces at the beginning.
     except ValueError:
         print("Error 404!!")
-#Remove spaces at the beginning.
-#Print the result.
+
