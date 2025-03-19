95c9257c1cb51cd8d4ad46d13ee9ea48f588ccbb
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 20:09:14 +0800

    Value Error

--- remove_spaces.py
+++ remove_spaces.py
@@ -1,3 +1,7 @@
 #Ask the user to input their full name with spaces.
+while True:
+    try:
+    except ValueError:
+        print("Error 404!!")
 #Remove spaces at the beginning.
 #Print the result.
