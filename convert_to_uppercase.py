3493f583cddb51a9d56755bc204566955e7dc54a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-19 21:09:03 +0800

    break function

--- convert_to_uppercase.py
+++ convert_to_uppercase.py
@@ -4,5 +4,6 @@ while True:
         pangalan = input("Enter your full name: ")
         #Print the result.
         print("Output: ", pangalan.upper()) #Convert all letters to uppercase
+        break
     except ValueError:
         print("ERROR 404!!!")
